3711622939dfae4ea1b76755b101c610d96540d8
Evans Musomi <evans.musomi@gmail.com>
2016-12-24 08:06:54 +0300

    Adds shebang and comment to words module

--- modularity/words.py
+++ modularity/words.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Retrieves and prints words from a URL.
 
 Usage:
@@ -49,4 +50,4 @@ def main(url):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1])  # The 0th arg is the module filename
