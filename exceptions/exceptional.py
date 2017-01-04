80bded2025e861094a577595541580c0ed261bf5
Evans Musomi <evans.musomi@gmail.com>
2017-01-04 12:07:16 +0300

    Prints exception message when invalid values provided

--- exceptions/exceptional.py
+++ exceptions/exceptional.py
@@ -1,11 +1,11 @@
 '''A module for demonstrating exceptions.'''
+import sys
 
 
 def convert(s):
     '''Converts to an integer.'''
-    x = -1
     try:
-        x = int(s)
-    except (ValueError, TypeError):
-        print("Conversion failed!")
-    return x
+        return int(s)
+    except (ValueError, TypeError) as e:
+        print("Conversion error: {}".format(str(e)), file=sys.stderr)
+        return -1
