172f89cd372526dae173369107d1032ed9b3e98f
Evans Musomi <evans.musomi@gmail.com>
2017-01-05 12:02:00 +0300

    Initializes TDD example

---
+++ tdd/text_analyzer.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TextAnalyzerTests(unittest.TestCase):
+    """Tests for the ``analyze_text()`` function."""
+
+    def test_function_runs(self):
+        """Basic smoke test: does the function run?"""
+        analyze_text()
+
+if __name__ == '__main__':
+    unittest.main()
